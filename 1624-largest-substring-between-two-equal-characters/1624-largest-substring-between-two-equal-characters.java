5061d475f915df7efdbf70305316e51f624b974c
Avinash Tanwar <tanwaravinash352@gmail.com>
2026-06-21 12:07:34 +0530

    Time: 2 ms (66.35%), Space: 42.9 MB (25.92%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
@@ -1,12 +1,13 @@
 class Solution {
     public int maxLengthBetweenEqualCharacters(String s) {
+        HashMap<Character,Integer> map1= new HashMap<>();
         int max1=-1;
         for(int i=0;i<s.length();i++){
-            for(int j=s.length()-1;j>=i+1;j--){
-                if(s.charAt(i)==s.charAt(j)){
-                    max1=Math.max(max1,j-i-1);
-                    break;
-                }
+            char c= s.charAt(i);
+            if(map1.containsKey(c)){
+                max1= Math.max(max1,i-map1.get(c)-1);
+            }else{
+                map1.put(c,i);
             }
         }
         return max1;
